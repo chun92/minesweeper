9cb0631ea9fb720697dee0b056a10b048990d6df
chun92 <chun92@postech.ac.kr>
2023-09-29 01:49:17 +0900

    fix: lint 어긴 사항 수정

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 const {Timestamp} = require("firebase-admin/firestore");
 
