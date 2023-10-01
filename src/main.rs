f263cca0c81c2b24a86f694bc27fdb6c2307a2c4
Chun <chunuiyu@gmail.com>
2023-10-01 16:01:26 +0900

    refactor: plugin 분리

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@ pub mod component {
 }
 
 pub mod system {
+    pub mod game;
     pub mod game_state;
     pub mod game_difficulty;
     pub mod mouse;
     pub mod timer;
     pub mod egui;
+    pub mod window;
 }
 
 pub mod core {
@@ -34,57 +36,11 @@ pub mod core {
 }
 
 use bevy::prelude::*;
-use system::game_state::GameState;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_state::<GameState>()
-        .init_resource::<asset::loader::TextureAtlasResource>()
-        .init_resource::<system::timer::platform::Timer>()
-        .init_resource::<component::mine::TotalMine>()
-        .init_resource::<component::mine::RemainingMine>()
-        .init_resource::<component::grid::Grid>()
-        .add_systems(Startup, asset::loader::setup)
-        .add_systems(PostStartup, core::init::camera::init)
-        .add_systems(OnEnter(GameState::Init), (
-            core::init::grid::clear,
-            core::init::grid::init.after(core::init::grid::clear),
-        ))
-        .add_systems(Update, (
-            system::mouse::mouse_events_system.after(system::egui::ui_system)
-                .run_if(system::game_state::run_if_all_menu_closed),
-            core::update::smiles::update,
-            core::update::time::update,
-        ))
-        .add_systems(OnEnter(GameState::Ready), (
-            core::update::cells::reset,
-            core::update::time::reset,
-        ))
-        .add_systems(Update, (
-            core::update::cells::texture_for_ready,
-            core::update::cells::first_click.after(core::update::cells::texture_for_ready),
-            core::update::mines::update_for_ready.after(core::update::cells::first_click),
-        )
-        .run_if(in_state(GameState::Ready)))
-        .add_systems(OnEnter(GameState::Playing), core::update::time::start)
-        .add_systems(Update, (
-            core::update::cells::update,
-            core::update::cells::texture_for_playing.after(core::update::cells::update),
-            core::update::mines::update_for_playing.after(core::update::cells::texture_for_playing),
-        ).run_if(in_state(GameState::Playing)))
-        .add_systems(OnEnter(GameState::Defeated), (
-            core::update::cells::bomb,
-            core::update::cells::texture_for_defeat.after(core::update::cells::bomb),
-            core::update::time::stop,
-            core::update::smiles::set_defeat,
-        ))
-        .add_systems(OnEnter(GameState::Win), (
-            core::update::cells::texture_for_win,
-            core::update::time::stop,
-            core::update::smiles::set_win,
-        ))
-        .insert_resource(system::game_difficulty::Difficulty::Hard)
+        .add_plugins(system::game::GamePlugin)
         .add_plugins(system::egui::EguiMenuPlugin)
         .run();
 }
---
+++ src/system/game.rs
@@ -0,0 +1,62 @@
+use bevy::prelude::*;
+
+use crate::asset;
+use crate::component;
+use crate::core;
+use crate::system;
+
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_state::<system::game_state::GameState>()
+            .init_resource::<asset::loader::TextureAtlasResource>()
+            .init_resource::<system::timer::platform::Timer>()
+            .init_resource::<component::mine::TotalMine>()
+            .init_resource::<component::mine::RemainingMine>()
+            .init_resource::<component::grid::Grid>()
+            .add_systems(Startup, asset::loader::setup)
+            .add_systems(PostStartup, core::init::camera::init)
+            .add_systems(OnEnter(system::game_state::GameState::Init), (
+                core::init::grid::clear,
+                core::init::grid::init.after(core::init::grid::clear),
+                system::window::init_window.after(core::init::grid::init),
+            ))
+            .add_systems(Update, (
+                system::mouse::mouse_events_system.after(system::egui::ui_system)
+                    .run_if(system::game_state::run_if_all_menu_closed),
+                core::update::smiles::update,
+                core::update::time::update,
+            ))
+            .add_systems(OnEnter(system::game_state::GameState::Ready), (
+                core::update::cells::reset,
+                core::update::time::reset,
+            ))
+            .add_systems(Update, (
+                core::update::cells::texture_for_ready,
+                core::update::cells::first_click.after(core::update::cells::texture_for_ready),
+                core::update::mines::update_for_ready.after(core::update::cells::first_click),
+            )
+            .run_if(in_state(system::game_state::GameState::Ready)))
+            .add_systems(OnEnter(system::game_state::GameState::Playing), core::update::time::start)
+            .add_systems(Update, (
+                core::update::cells::update,
+                core::update::cells::texture_for_playing.after(core::update::cells::update),
+                core::update::mines::update_for_playing.after(core::update::cells::texture_for_playing),
+            ).run_if(in_state(system::game_state::GameState::Playing)))
+            .add_systems(OnEnter(system::game_state::GameState::Defeated), (
+                core::update::cells::bomb,
+                core::update::cells::texture_for_defeat.after(core::update::cells::bomb),
+                core::update::time::stop,
+                core::update::smiles::set_defeat,
+            ))
+            .add_systems(OnEnter(system::game_state::GameState::Win), (
+                core::update::cells::texture_for_win,
+                core::update::time::stop,
+                core::update::smiles::set_win,
+            ))
+            .insert_resource(system::game_difficulty::Difficulty::Hard);
+    }
+}
