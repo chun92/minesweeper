6cb2f367b69ff87cc0eb64845e8c6e57f5e48489
chun92 <chun92@postech.ac.kr>
2023-10-05 23:27:34 +0900

    feat: login firebase 서버 구현

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,57 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 const {Timestamp} = require("firebase-admin/firestore");
+const axios = require("axios");
+const fs = require("fs");
+const yaml = require("js-yaml");
 
 admin.initializeApp();
 const db = admin.firestore();
+
+exports.oauth = onRequest(async (request, response) => {
+  const state = request.query.state;
+  if (!state) {
+    response.status(400).send("Missing state parameter");
+    return;
+  }
+
+  const code = request.query.code;
+  if (!code) {
+    response.status(400).send("Missing auth code");
+    return;
+  }
+
+  try {
+    const configFile = fs.readFileSync("config.yaml", "utf8");
+    const config = yaml.load(configFile);
+
+    const clientId = config.settings.client_id;
+    const clientSecret = config.settings.client_secret;
+    const redirectUri = config.settings.redirect_uri;
+
+    const tokenResponse = await axios.post("https://oauth2.googleapis.com/token", {
+      code: code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      grant_type: "authorization_code",
+    });
+    const accessToken = tokenResponse.data.access_token;
+    const userInfoResponse = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+      },
+    });
+
+    const email = userInfoResponse.data.email;
+    const docRef = admin.firestore().collection("login").doc(state);
+    await docRef.set({user_id: email});
+    response.send("Successfully fetched user information");
+  } catch (error) {
+    console.error("Error fetching user information:", error);
+    response.status(500).send("Failed to fetch user information");
+  }
+});
 
 exports.uploadRanking = onRequest(async (req, res) => {
   const {nickname, difficulty, time} = req.body;
